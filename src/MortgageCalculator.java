f64f0d532bcbc48ca8e7b89c28079857e8f46686
Deathshadowown <shanervanniekerk@gmail.com>
2020-08-25 18:09:29 +0200

    updated maths

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -18,14 +18,19 @@ import java.text.NumberFormat;
 public class MortgageCalculator{
     public static void main(String[] args){
         NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.US);
+        final byte MOTHS_IN_YEAR = 100;
+        final byte PERSENT = 12;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal: "); // amount of loan you want to get
         long principal = scanner.nextLong();
         System.out.print("Annual Interest Rate: ");
         double annualInterestRate = scanner.nextDouble();
+        double monthlyIntrests = annualInterestRate / PERSENT / MOTHS_IN_YEAR;
         System.out.print("Period (Years): "); // loan to pay back in X amount of years
         int periodYears = scanner.nextByte();
-        double mortgage = principal * ((annualInterestRate /= 100) / 12) * (Math.pow(1 + annualInterestRate, periodYears) / (Math.pow(annualInterestRate + 1, periodYears) - 1));
+        int amoutOfPayments = periodYears / MOTHS_IN_YEAR;
+        // double mortgage = principal * ((annualInterestRate /= 100) / 12) * (Math.pow(1 + annualInterestRate, periodYears) / (Math.pow(annualInterestRate + 1, periodYears) - 1));
+        double mortgage = principal * (monthlyIntrests * Math.pow(1 + monthlyIntrests, amoutOfPayments)) / (Math.pow(1 + monthlyIntrests, amoutOfPayments));
         System.out.println("Mortgage: "+ formatter.format(mortgage));
     }
 }
